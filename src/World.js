a05de4509e0e7b3fe51e41747dd797933a4f900c
Andres Pulecio <andres.pulecio@blackboard.com>
2022-07-26 21:18:11 -0500

    feat: path to center

--- src/World.js
+++ src/World.js
@@ -226,7 +226,7 @@ class world {
         stoneBig2.init('../models/stoneBig.glb', scene, world, normalMaterial, q, 1, -12, 0, -4, 0.8, 0.8, 0.8, 0, 1);
         const stoneBig3 = new importModels();
         stoneBig3.init('../models/stoneBig.glb', scene, world, normalMaterial, q, 1, 4, 0, -8, 0.8, 0.8, 0.8, 0, 1);
-        stoneBig3.init('../models/stoneBig.glb', scene, world, normalMaterial, q, 1, 5, 0, 21, 0.8, 0.8, 0.8, 0, 1);
+        stoneBig3.init('../models/stoneBig.glb', scene, world, normalMaterial, q, 1, 5, 0, 22, 0.8, 0.8, 0.8, 0, 1);
         
         const stoneMedium1 = new importModels();
         stoneMedium1.init('../models/stoneMedium.glb', scene, world, normalMaterial, q, 2, -10, 0, 2, 0.1, 0.1, 0.1, 0, 1);
@@ -256,15 +256,20 @@ class world {
         const groupMushrooms = new importModels();
         groupMushrooms.init('../models/groupMushrooms.glb', scene, world, normalMaterial, q, 8, 12, 0, 3, 0.6, 1, 0.6, 0, 1);
         
-        //Path start
+        //Path start to center
         const tile1 = new importModels();
         tile1.init('../models/tile.glb', scene, world, normalMaterial, q, 1, -1, 0, 9, 1, 0.1, 1, 0, 1);
         tile1.init('../models/tile.glb', scene, world, normalMaterial, q, 1, -1.2, 0, 15.2, 1, 0.1, 1, 0, 1);
         tile1.init('../models/tile.glb', scene, world, normalMaterial, q, 1, 1.8, 0, 11.3, 1, 0.1, 1, 0, 1);
         stoneSmall3.init('../models/stoneSmall.glb', scene, world, normalMaterial, q, 3, -2, 0, 17, 0.1, 0.1, 0.1, 0, 1);
         
-        //name
-        var xPosition = -17;
+        for (var i=0; i<10; i=i+1.2) {
+            zPosition = 27;
+            tile1.init('../models/tile.glb', scene, world, normalMaterial, q, 1, -2 + getRndInteger(1, 5), 0, zPosition + (i*3), 1, 0.1, 1, 0, 1);
+        }
+        
+        //Name
+        var xPosition = -18;
         // var yPosition = ;
         var zPosition = 18;
         var modelscale = 1;
@@ -302,7 +307,7 @@ class world {
         O_Word.init('../models/O.glb', scene, world, normalMaterial, q, modelscale, xPosition + xScale * 25.3 , 1, zPosition, xScale, yScale, zScale, modelMass, rotation);
         
         const DEVOPS_Word = new importModels();
-        DEVOPS_Word.init('../models/DEVOPS.glb', scene, world, normalMaterial, q, modelscale/2, xPosition + 22 , 1.5, zPosition + 4, 3.5, 0.3, 1, modelMass, rotation);
+        DEVOPS_Word.init('../models/DEVOPS.glb', scene, world, normalMaterial, q, modelscale/2, xPosition + 24 , 1.5, zPosition + 4, 3.5, 0.3, 1, modelMass, rotation);
         // Wall start left
         var xPosition = -16;
         // var yPosition = ;
@@ -396,9 +401,12 @@ class world {
             sunlight.position.copy(chassisBody.position).add(lightOffset);
             plane.position.copy(chassisBody.position).add(planeOffset);
             plane.position.copy(chassisBody.position).add(planeOffset);
+
         }
-            
+        
         function render() {
+            console.log(chassisBody.position);
+            
             requestAnimationFrame(render);
             renderer.render(scene, camera);
             updatePhysics();
@@ -502,7 +510,10 @@ class world {
                 break;
             }
         }
-
+        function getRndInteger(min, max) {
+            return Math.floor(Math.random() * (max - min) ) + min;
+        }
+        
         window.addEventListener('keydown', navigate)
         window.addEventListener('keyup', navigate)
 
