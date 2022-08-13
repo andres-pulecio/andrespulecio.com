b2d45b5b02893117ac056da06e6cca3757c00ab8
andres-pulecio <andresfelipepulecio@gmail.com>
2022-08-13 13:31:56 -0500

    feat: add push to my personal registry

--- deploy/aws-deploy.sh
+++ deploy/aws-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/dash
-echo "********************* Login AWS REGISTRY *********************"
+echo "********************* Docker build Personal Registry *********************"
+docker build . -t andrespulecio/my-portfolio
+echo "********************* Docker push Personal Registry *********************"
+docker push andrespulecio/my-portfolio:latest
+echo "********************* Login AWS Registry *********************"
 token=`aws ecr-public get-login-password --region us-east-1`
 docker login --username AWS --password $token public.ecr.aws/z6s0c3o0
 echo "********************* npm build *********************"
