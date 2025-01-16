26d9ff8513220126d567205d0d990418eb989246
andres-pulecio <andresfelipepulecio@gmail.com>
2025-01-15 21:34:13 -0500

    deploy: kill

---
+++ deploy/aws-kill.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# List all resources with the tag "my-portfolio"
+data "aws_vpc" "my_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-portfolio"]
+  }
+}
+
+data "aws_subnet" "my_subnet" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-portfolio"]
+  }
+}
+
+data "aws_security_group" "my_security_group" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-portfolio"]
+  }
+}
+
+data "aws_ecs_cluster" "my_cluster" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-portfolio"]
+  }
+}
+
+data "aws_ecs_service" "my_service" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-portfolio"]
+  }
+}
+
+# Delete ECS service
+resource "aws_ecs_service" "my_service_delete" {
+  count = length(data.aws_ecs_service.my_service.arns)
+  cluster = data.aws_ecs_service.my_service[count.index].cluster_arn
+  name    = data.aws_ecs_service.my_service[count.index].service_name
+
+  force_delete = true
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Delete ECS cluster
+resource "aws_ecs_cluster" "my_cluster_delete" {
+  count = length(data.aws_ecs_cluster.my_cluster.arns)
+  cluster_name = data.aws_ecs_cluster.my_cluster[count.index].name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Delete Security Group
+resource "aws_security_group" "my_security_group_delete" {
+  count = length(data.aws_security_group.my_security_group.ids)
+  security_group_id = data.aws_security_group.my_security_group[count.index].id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Delete Subnet
+resource "aws_subnet" "my_subnet_delete" {
+  count = length(data.aws_subnet.my_subnet.ids)
+  subnet_id = data.aws_subnet.my_subnet[count.index].id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Delete VPC
+resource "aws_vpc" "my_vpc_delete" {
+  count = length(data.aws_vpc.my_vpc.ids)
+  vpc_id = data.aws_vpc.my_vpc[count.index].id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
