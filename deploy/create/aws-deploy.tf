671eb30740467ff01e912c3ee40e4d4f8518fd3c
andres-pulecio <andresfelipepulecio@gmail.com>
2025-01-16 15:37:42 -0500

    deplot: Igt

--- deploy/create/aws-deploy.tf
+++ deploy/create/aws-deploy.tf
@@ -14,6 +14,35 @@ resource "aws_vpc" "my-portfolio" {
   }
 }
 
+# Define an Internet Gateway
+resource "aws_internet_gateway" "my-portfolio" {
+  vpc_id = aws_vpc.my-portfolio.id
+
+  tags = {
+    Name = "my-portfolio"
+  }
+}
+
+# Define a Route Table
+resource "aws_route_table" "my-portfolio" {
+  vpc_id = aws_vpc.my-portfolio.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-portfolio.id
+  }
+
+  tags = {
+    Name = "my-portfolio"
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "my-portfolio" {
+  subnet_id      = aws_subnet.my-portfolio.id
+  route_table_id = aws_route_table.my-portfolio.id
+}
+
 # Define a Subnet
 resource "aws_subnet" "my-portfolio" {
   vpc_id                  = aws_vpc.my-portfolio.id
@@ -45,73 +74,4 @@ resource "aws_security_group" "my-portfolio" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "my-portfolio"
-  }
-}
-
-# Define an ECS cluster
-resource "aws_ecs_cluster" "default" {
-  name = "my-portfolio"  # Name of the ECS cluster
-
-  tags = {
-    Name = "my-portfolio"
-  }
-}
-
-# Define an ECS task definition
-resource "aws_ecs_task_definition" "task" {
-  family                   = "my-portfolio"  # Name of the task definition family
-  container_definitions    = jsonencode([
-    {
-      name  = "my-portfolio",  # Name of the container
-      image = "public.ecr.aws/z6s0c3o0/my-porfolio:latest",  # URL of the container image in ECR
-      essential = true,  # Specify if the container is essential
-      memory = 512,  # Memory allocated to the container
-      cpu    = 256,  # CPU units allocated to the container
-      portMappings = [{
-        containerPort = 3000,  # Port number on the container
-        hostPort      = 3000,  # Port number on the host
-      }],
-    },
-  ])
-  requires_compatibilities = ["FARGATE"]  # Specify that the task runs on Fargate
-  network_mode             = "awsvpc"  # Network mode for the task
-  memory                   = "512"  # Memory allocated to the task
-  cpu                      = "256"  # CPU units allocated to the task
-
-  tags = {
-    Name = "my-portfolio"
-  }
-}
-
-# Define an ECS service
-resource "aws_ecs_service" "service" {
-  name            = "my-portfolio"  # Name of the ECS service
-  cluster         = aws_ecs_cluster.default.id  # ID of the ECS cluster
-  task_definition = aws_ecs_task_definition.task.arn  # ARN of the task definition
-  desired_count   = 1  # Number of tasks to run
-  launch_type     = "FARGATE"  # Specify that the service runs on Fargate
-
-  # Configure the network settings for the service
-  network_configuration {
-    subnets          = [aws_subnet.my-portfolio.id]  # Use the Subnet ID created
-    security_groups  = [aws_security_group.my-portfolio.id]  # Use the Security Group ID created
-    assign_public_ip = true  # Assign a public IP to the tasks
-  }
-
-  tags = {
-    Name = "my-portfolio"
-  }
-}
+    cid
